1a7f059bccac31ab5ef87b8a71d4ac07c0f2d96f
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-03-29 21:13:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp4;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            p1.Nome = Console.ReadLine();
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            p2.Nome = Console.ReadLine();
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            } else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+        }
+    }
+}
